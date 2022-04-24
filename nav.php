05134643185e6872d7113b498a378ad4dbd9a03c
filipe <filipedavila6@gmail.com>
2022-04-24 17:36:08 +0100

    correcao do titulo do nav

--- nav.php
+++ nav.php
@@ -89,7 +89,7 @@
     </li>
       <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-              Prefências
+              Preferências
           </a>
           <div class="dropdown-menu">
               <a class="dropdown-item" href="index.php?page=voluntario_settings">Dados Pessoais</a>
